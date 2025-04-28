0d8b98d9f114ff97c4ab586d99e15564a494616c
manspach <madison.anspach22@kzoo.edu>
2025-04-28 13:46:48 -0400

    added code to correctly implement name is empty test

--- InputValidator.java
+++ InputValidator.java
@@ -1,12 +1,11 @@
 /*
  * This is the InputValidator class. Please complete this class with
- * appropiate JavaDoc comments, method and code comments, and the appropiate
+ * appropriate JavaDoc comments, method and code comments, and the appropriate
  * methods to validate inputs from the user. 
  */
 
 public class InputValidator 
 {
-    
     public static boolean validateNameField(String name)
     {
         // check if name is not empty
