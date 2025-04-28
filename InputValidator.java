72cecdb2e736868c9d6e585e9bdb09cdd7aabab5
manspach <madison.anspach22@kzoo.edu>
2025-04-28 14:00:35 -0400

    copied necessary files from main into personal branch

--- InputValidator.java
+++ InputValidator.java
@@ -1,11 +1,15 @@
 /*
  * This is the InputValidator class. Please complete this class with
- * appropiate JavaDoc comments, method and code comments, and the appropiate
+ * appropriate JavaDoc comments, method and code comments, and the appropriate
  * methods to validate inputs from the user. 
  */
 
-public class InputValidator 
-{
-    
-
-}
+ public class InputValidator 
+ {
+     public static boolean validateNameField(String name)
+     {
+         // check if name is not empty
+         return name != null && !name.isEmpty();
+     }
+ 
+ }
