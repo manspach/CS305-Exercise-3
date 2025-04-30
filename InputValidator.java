3b9550d187172fcbfa797393ea388b3f0514d9b6
manspach <madison.anspach22@kzoo.edu>
2025-04-30 18:41:21 -0400

    refactoring validateLastNameField to check if null, if at least two characters, and if string is in the language defined by the regular expression

--- InputValidator.java
+++ InputValidator.java
@@ -9,18 +9,24 @@ public class InputValidator
     public static boolean validateNameField(String name)
     {
         // regex explanation:
-        // ^\\p{L}+ : starts with one or more letters
+        // \\p{L}+ : starts with one or more letters
         // ( \\p{L}+)? : optionally followed by one space and more letters
-        // $ : end of string
         return name != null
             && name.length() >= 2
-            && name.matches("^\\p{L}+( \\p{L}+)?$");
+            && name.matches("\\p{L}+( \\p{L}+)?");
     }
 
     public static boolean validateLastNameField(String lastName)
     {
-        // check if lastName is not null and not empty
-        return lastName != null && !lastName.isEmpty() && lastName.length() >= 2;
+        // regex explanation:
+        // [ʻ]? : optionally begin with one 'okina
+        // [\\p{L}]+ : followed by at least one letter (could be beginning of string)
+        // [ ʻ'\\-]? : optionally followed by one space, one 'okina, one apostrophe, or one hypen
+        // [\\p{L}]+ : must be followed by at least one letter
+        // ([ ʻ'\\-]?[\\p{L}]+)* : entire second statement happening zero or more times
+        return lastName != null
+            && lastName.length() >= 2
+            && lastName.matches("[ʻ]?[\\p{L}]+([ ʻ'\\-]?[\\p{L}]+)*");
     }
 
 }
