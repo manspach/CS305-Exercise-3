a1a53a4b7bf026d3c1be5435082247d183a77553
manspach <madison.anspach22@kzoo.edu>
2025-04-30 19:56:04 -0400

    editing regular expressions for validateNameField and validateLastNameField

--- InputValidator.java
+++ InputValidator.java
@@ -9,24 +9,26 @@ public class InputValidator
     public static boolean validateNameField(String name)
     {
         // regex explanation:
+        // ^ : start
         // \\p{L}+ : starts with one or more letters
         // ( \\p{L}+)? : optionally followed by one space and more letters
+        // $ : stop
         return name != null
             && name.length() >= 2
-            && name.matches("\\p{L}+( \\p{L}+)?");
+            && name.matches("^[\\p{L}]+( [\\p{L}]+)?$");
     }
 
     public static boolean validateLastNameField(String lastName)
     {
         // regex explanation:
-        // [ʻ]? : optionally begin with one 'okina
-        // [\\p{L}]+ : followed by at least one letter (could be beginning of string)
-        // [ ʻ'\\-]? : optionally followed by one space, one 'okina, one apostrophe, or one hypen
+        // ^ : start
+        // [\\p{L}]+ : at least one letter
+        // [ '\\-]? : optionally followed by one space, one apostrophe, or one hypen
         // [\\p{L}]+ : must be followed by at least one letter
-        // ([ ʻ'\\-]?[\\p{L}]+)* : entire second statement happening zero or more times
+        // ([ '\\-]?[\\p{L}]+)* : entire second statement happening zero or more times
         return lastName != null
             && lastName.length() >= 2
-            && lastName.matches("[ʻ]?[\\p{L}]+([ ʻ'\\-]?[\\p{L}]+)*");
+            && lastName.matches("^[\\p{L}]+([ '\\-]?[\\p{L}]+)*$");
     }
 
 }
