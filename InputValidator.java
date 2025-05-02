4f241183cf3a8f27ea0c7a5733eee50bc6da29d9
CollinR924 <collin.russell23@kzoo.edu>
2025-05-02 12:37:52 -0400

    Added documentation in InputValidator.Java

--- InputValidator.java
+++ InputValidator.java
@@ -47,6 +47,16 @@ public class InputValidator
             && email.matches("^[^\\s@]+@[^\\s@]+(\\.[^\\s@]+)+$");
     }
 
+    /**
+     * Validates the title input from user 
+     * 
+     * The validation ensures: 
+     *     The title does not have a {@code null}, has a length longer than 0, and its length is not longer than 100 characters.
+     * 
+     * @param title is the title validated
+     * @return {@code true} the title is within limits, {@code false} the title is not within limits.
+     */
+
     //Checks if the tile is empty 
     public static boolean validateTitle(String title)
     {
@@ -55,6 +65,16 @@ public class InputValidator
           && title.trim().length() < 100;
     }
 
+
+    /**
+     * Validates the description input from user 
+     * 
+     * The validation ensures: 
+     *     The description does not have a {@code null}, has a length longer than 0, and its length is not longer than 100 characters.
+     * 
+     * @param title is the description validated
+     * @return {@code true} the description is within limits, {@code false} the description is not within limits.
+     */
     //Checks if description is empty 
     public static boolean validateDescription(String description)
     {
@@ -64,6 +84,18 @@ public class InputValidator
            description.trim().length() < 1000;
     }
 
+    /**
+     * Validates the image given to the system
+     * 
+     * The validation ensures: 
+     *      The image has an extension of ".JPG" or ".PNG"
+     *      Ensures the fileSeize is not more than 4MB
+     * 
+     * @param fileName the name of the file that is is checked for the extention. 
+     * @param fileSize the size of the file given to they system. 
+     * @return {@code true} the Image has the right extention and less than file size limit.
+     *         {@code false} the Image has the wrong extention and/or has a file size over the limit.
+     */
     //Checks if the image has the proper extension (".JPG" or ".PNG") and if the file size is less the 4 MB
     public static boolean validateImage(String fileName, double fileSize)
     {
@@ -72,6 +104,19 @@ public class InputValidator
         return fileName.endsWith(".JPG") || fileName.endsWith(".PNG");
     }
 
+
+    /**
+     * Validates the dimentions of the work 
+     * 
+     * The validation ensures: 
+     *      The dimensions given include a width, height, and unit. 
+     * 
+     * @param width the first number of the dimensions given 
+     * @param height the second number of the dimensions given 
+     * @param unit the unit of measure of the dimentions (i.e meter, inch, feelt, etc)
+     * @return {@code true} the dimentions has a value in all fields.
+     *         {@code false} the dimentions does not have all of the fields filled. 
+     */
     //Checks if there is a valid dimention of the art (width, height) and there is a valid unit of measure of the art (meter, inch, feet, etc)
     public static boolean isValidDimensions(String width, String height, String unit)
     {
